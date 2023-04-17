b738940b00c45a5712843fb08e21d2f868b2b9ed
Eric James <urbanscene@gmail.com>
2023-04-17 11:01:38 -0500

    Edited button only comes up after editing

--- src/Elements/InputField.js
+++ src/Elements/InputField.js
@@ -10,12 +10,16 @@ const InputField = ({ setDataSheets, dataSheet, fieldIndex }) => {
 
     const [originalValue, setOriginalValue] = useState(field.value);
 
+    const [isEdited, setIsEdited] = useState(false);
+
+
     // Only show certain fields. The data still gets calculated in state 
     if (field.type === "hidden" || field.type === "total_both" || field.type === "total_low" || field.type === "total_high") return null;
 
     // console.log("INPUTFIELD", field);
 
     function onChange(e) {
+        setIsEdited(true);
         dataSheet.fields[fieldIndex].value = e.target.value;
 
         setDataSheets((state) => {
@@ -25,6 +29,7 @@ const InputField = ({ setDataSheets, dataSheet, fieldIndex }) => {
     }
 
     function revertValue() {
+        setIsEdited(false);
         dataSheet.fields[fieldIndex].value = originalValue;
 
         setDataSheets(state => {
@@ -47,7 +52,7 @@ const InputField = ({ setDataSheets, dataSheet, fieldIndex }) => {
                 {field.type === "editable" && field.fieldType === "text" ? <input comma={field.comma} type={field.fieldType} min={field.minValue || 0} max={field.maxValue || 1000000} step={field.stepValue || 1} maxLength={maxLength} value={field.value} onChange={onChange} disabled={field.type === "locked" || field.type === "inline_total"} /> : null}
                 {field.type === "editable" && field.fieldType === "number" ? <NumericFormat thousandSeparator="," min={field.minValue || 0} max={field.maxValue || 1000000} step={field.stepValue || 1} maxLength={maxLength} value={field.value} onChange={onChange} disabled={field.type === "locked" || field.type === "inline_total"} /> : null}
 
-                {field.type === "editable" ? <button className="revert" title="Revert" onClick={revertValue}>⟳</button> : null}
+                {field.type === "editable" && isEdited ? <button className="revert" title="Revert" onClick={revertValue}>⟳</button> : null}
                 {field.type === "locked" || field.type === "inline_total" ? <DisplayedNumber value={field.value} valueType={field.valueType} /> : null}
             </div>
             {/* {field.value} {field.valueType} */}
